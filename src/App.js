e0c0e590d7ab473ff65bb18bb09afe45e9511cbf
Sidney Tan <sidneyjt@uci.edu>
2021-03-23 00:05:02 -0700

    Set up project structure.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import globalState from "./redux/reducers/globalReducers";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Content from "./components/Content";
+import "./App.css";
 function App() {
+  const globalStore = createStore(globalState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={globalStore}>
+        <Content></Content>
+      </Provider>
     </div>
   );
 }
